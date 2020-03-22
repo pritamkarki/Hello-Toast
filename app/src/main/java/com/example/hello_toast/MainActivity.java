57fe8fabed80c6cbac6b8097678768f0ef4f6186
Pritam Karki <P.Karki6737@student.leedsbeckett.ac.uk>
2020-03-22 15:09:19 +0545

    hello toast

---
+++ app/src/main/java/com/example/hello_toast/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.hello_toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+    private Button countButton;
+    private Button toastButton;
+
+
+    private int mCount = 0;
+
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+
+        Toast.makeText(this, "Toast", Toast.LENGTH_SHORT).show();
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+
+    }
+}
